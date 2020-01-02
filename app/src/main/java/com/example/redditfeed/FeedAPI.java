a3787efcc73fc23ae10c79df53be850e6bc13aea
Michał <switalski.michal.97@gmail.com>
2020-01-02 11:35:35 +0100

    first commit

---
+++ app/src/main/java/com/example/redditfeed/FeedAPI.java
@@ -0,0 +1,14 @@
+package com.example.redditfeed;
+
+import com.example.redditfeed.model.Feed;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface FeedAPI {
+
+    String BASE_URL = "https://www.reddit.com/r/";
+
+    @GET("witcher/.rss")
+    Call<Feed> getFeed();
+}
